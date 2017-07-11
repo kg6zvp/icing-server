96fe722eb8feafc5ccdea969bd5b606e866e4c61
Sam McCollum <smccollum@mccollum.enterprises>
2017-07-10 23:38:00 -0700

    Fix file streaming for local filesystems

--- src/main/java/enterprises/mccollum/home/media/jax/RawMediaJax.java
+++ src/main/java/enterprises/mccollum/home/media/jax/RawMediaJax.java
@@ -1,16 +1,28 @@
 package enterprises.mccollum.home.media.jax;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.commons.vfs2.FileContent;
+import org.apache.commons.vfs2.FileSystemException;
+import org.apache.commons.vfs2.RandomAccessContent;
+import org.apache.commons.vfs2.VFS;
+import org.apache.commons.vfs2.util.RandomAccessMode;
+
+import com.github.amr.mimetypes.MimeTypes;
 
 import enterprises.mccollum.home.media.model.FilePathCodec;
+import enterprises.mccollum.home.media.model.MediaSource;
+import enterprises.mccollum.home.media.model.MediaSourceDao;
 
 /**
  * Provides raw access to files
@@ -24,14 +36,48 @@ public class RawMediaJax {
 	@Inject
 	FilePathCodec pathCodec;
 	
+	@Inject
+	MediaSourceDao sources;
+	
+	@HeaderParam("Range")
+	String rangeHeader;
+	
 	@GET
 	@Path("{source}/{path}")
-	@Produces(MediaType.APPLICATION_OCTET_STREAM)
-	public Response getFile(@PathParam("source")String source, @PathParam("path")String filePathEncoded){
+	public Response getFile(@PathParam("source")String sourceName, @PathParam("path")String filePathEncoded){
 		String filePath = pathCodec.decodePath(filePathEncoded);
-		String basePath = System.getProperty("media.filepath.base");
-		System.out.println("Getting "+basePath+filePath);
-		File file = new File(basePath+filePath);
-		return Response.ok(file).build();
+		MediaSource src = new MediaSource();
+		src.setName(sourceName);
+		src = sources.getMatching(src).get(0);
+		
+		String fileExtention = filePath.substring(filePath.lastIndexOf(".")+1);
+		
+		try {
+			ResponseBuilder builder = null;
+			if(src.getProtocol().equals("file")){
+				builder = Response.ok(new File(src.getBasePath()+"/"+filePath));
+			}else{
+				FileContent file = VFS.getManager().resolveFile(src.getCompleteUrl()+"/"+filePath).getContent();
+				RandomAccessContent fContents = file.getRandomAccessContent(RandomAccessMode.READ);
+				fContents.seek(parseRangeIgnoreEnd());
+				builder = Response.ok(fContents.getInputStream())
+								.header("Content-Length", file.getSize());
+			}
+			return builder.header("Content-Type", MimeTypes.getInstance().getByExtension(fileExtention).getMimeType())
+						.header("Accept-Ranges", "bytes")
+						.build();
+		} catch (FileSystemException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return Response.status(Status.NOT_FOUND).build();
+	}
+	
+	private long parseRangeIgnoreEnd(){
+		if(rangeHeader == null)
+			return 0L;
+		String range[] = rangeHeader.split("=")[1].split("-");
+		return Long.parseLong(range[0]);
 	}
 }
