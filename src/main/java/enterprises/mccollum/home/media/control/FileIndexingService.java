a8bf360d6e51a838af1c2e8e9f3603cad698a4c4
James Solum <jsolum@mccollum.enterprises>
2017-08-04 23:34:28 -0700

    adds ability to browse media files of a source, closes #10

--- src/main/java/enterprises/mccollum/home/media/control/FileIndexingService.java
+++ src/main/java/enterprises/mccollum/home/media/control/FileIndexingService.java
@@ -3,13 +3,22 @@ package enterprises.mccollum.home.media.control;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
 
 import org.apache.commons.lang3.NotImplementedException;
 
 import enterprises.mccollum.home.media.model.MediaSource;
+import enterprises.mccollum.home.media.model.MediaSourceDao;
+
 import static enterprises.mccollum.home.media.control.MediaFileUtils.getMimeTypeByFilePath;
 
 public class FileIndexingService {
+	@Inject
+	MediaSourceDao mediaSources;
+	
 	/**
 	 * Pass in a mediasource to get a list of media files inside it and their associated mimetypes
 	 * 
@@ -23,17 +32,40 @@ public class FileIndexingService {
 		doSearch(files, src, "");
 		return files;
 	}
+	
+	/**
+	 * Pass in a mediasource to get a list of media files inside it and their associated mimetypes
+	 * 
+	 * Output is a map like so: Map<FilePathRelativeToSourceBaseDir, MimeType>
+	 * 
+	 * @param sourceName The name of the media source to scan for content
+	 * @return Map<String, String> key:value pairs of relative file path and associated mime type
+	 */
+	public Map<String, String> search(String sourceName){
+		return search(mediaSources.getByName(sourceName));
+	}
 
+	private String directoryHelper(MediaSource src, String baseDir){
+		return src.getBasePath()+baseDir;
+	}
+	
 	private void doSearch(Map<String, String> files, MediaSource src, String baseDir){
+		Logger.getLogger(getClass().getSimpleName()).log(Level.FINEST,
+				String.format("Indexing %s: %s", src.getName(), directoryHelper(src, baseDir)));
 		if(src.getProtocol().equals("file")){
-			File dir = new File(src.getBasePath()+baseDir);
-			if(!dir.exists() || dir.isFile())
+			File dir = new File(directoryHelper(src, baseDir));
+			if(!dir.exists() || dir.isFile()){
+				Logger.getLogger(getClass().getSimpleName()).log(Level.INFO,
+						String.format("Bugging out because dir.exists() = %b", dir.exists()));
+				Logger.getLogger(getClass().getSimpleName()).log(Level.INFO,
+						String.format("Bugging out because dir.isFile = %b", dir.isFile()));
 				return;
+			}
 			for(File f : dir.listFiles()){
 				if(f.isDirectory()){
-					doSearch(files, src, baseDir+f.getName()+"/");
+					doSearch(files, src, baseDir+"/"+f.getName());
 				}else{
-					files.put(baseDir+f.getName(), getMimeTypeByFilePath(f.getName()));
+					files.put(baseDir+"/"+f.getName(), getMimeTypeByFilePath(f.getName()));
 				}
 			}
 		}else{
---
+++ src/main/java/enterprises/mccollum/home/media/jsf/FileIndexView.java
@@ -0,0 +1,87 @@
+package enterprises.mccollum.home.media.jsf;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import enterprises.mccollum.home.media.control.FileIndexingService;
+
+@Named
+//@ConversationScoped
+@RequestScoped
+@Stateful(passivationCapable=true)
+public class FileIndexView {
+	@Inject 
+	FileIndexingService fileService;
+	
+	String sourceName;
+	
+	@PostConstruct
+	public void init(){
+		sourceName = getParam("source");
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @return a list of relative paths to the media files which can be used to generate links
+	 */
+	public List<MediaData> getMediaFileList(){
+		Map<String, String> foundFiles = fileService.search(sourceName);
+		List<MediaData> videoFiles = new LinkedList<MediaData>();
+		for(Map.Entry<String, String> file : foundFiles.entrySet()){
+			if(file.getValue().startsWith("video/")) {
+				try {
+				videoFiles.add(new MediaData(file.getKey(), sourceName));
+				} catch (UnsupportedEncodingException e) {
+					Logger.getLogger(getClass().getSimpleName()).log(Level.WARNING, String.format("Failed while URL encoding %s in %s", file.getKey(), sourceName));
+//					e.printStackTrace();
+				}
+			}
+		}
+		return videoFiles;
+	}
+
+	private String getParam(String paramName){
+		return FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get(paramName);
+	}
+	
+	public String getSourceName(){
+		return sourceName;
+	}
+	
+	
+	public static class MediaData{
+		private final String filePath;
+		private final String fileLink;
+		
+		public MediaData(String filePath, String sourceName) throws UnsupportedEncodingException{
+			this.filePath = filePath;
+			this.fileLink = FacesContext.getCurrentInstance().getExternalContext().getApplicationContextPath()+"/web/raw/"+sourceName+"?filePath="+
+			URLEncoder.encode(filePath, "UTF-8");
+		}
+		
+		public String getFilePath(){
+			return filePath;
+		}
+		
+		public String getFileLink(){
+			return fileLink;
+		}
+		
+		public String getFileName(){
+			return filePath.substring(filePath.lastIndexOf('/')+1);
+		}
+	}
+}
